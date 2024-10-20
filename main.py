a6f2d23136a4b7a0ac0fd2894006a1635fe4af78
thananchanok6787042 <thananchanok.chu@student.mahidol.edu>
2024-10-20 14:40:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# YOUR CODE HERE
+n = int(input())
+list1=[]
+list2= []
+for i in range(n):
+    x = int(input())
+    list1.append(x)
+for j in range(n):   
+    y = int(input())
+    list2.append(y)
+
+def summation():
+    updated_list =[]
+    for char in range(n):
+        updated_list.append(list1[char]+list2[char])
+    return updated_list
+
+def find_min_max():
+    minn = min(summation())
+    maxx = max(summation())
+    return(minn,maxx)
+
+print(summation())
+print(find_min_max())        
